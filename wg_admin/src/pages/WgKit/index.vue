36b86d52434f58b520d359f1b8501c6b3c988616
brunothelima <brunothelima@gmail.com>
2019-03-29 17:42:00 -0300

    Crop algorithms

--- wg_admin/src/pages/WgKit/index.vue
+++ wg_admin/src/pages/WgKit/index.vue
@@ -6,11 +6,7 @@
       <text-demo />
       <buttons-demo />
       <page-settings />
-      <img src="http://localhost:8081/?img=image_before.webp&ratio=4:3&width=300&attention=true" alt="">
-      <img src="http://localhost:8081/?img=image_before.webp&ratio=16:9&width=300&attention=true" alt="">
-      <img src="http://localhost:8081/?img=image_before.webp&ratio=3:4&height=225&attention=true" alt="">
-      <img src="http://localhost:8081/?img=image_before.webp&ratio=9:16&width=300&attention=true" alt="">
-      <img src="http://localhost:8081/?img=image_before.webp&ratio=1:1&width=300&attention=true" alt="">
+      <wg-img src="image_before.webp" :height="200" :width="200" :x="230" :y="460">My caption</wg-img>
     </container>
   </div>
 </template>
@@ -37,6 +33,9 @@ export default {
   padding: 2rem 0;
   section, .page-settings{
     margin-bottom: 10rem;
+  }
+  /deep/ figure {
+    margin: 2rem;
   }
 }
 </style>
--- wg_api/image_crop.js
+++ wg_api/image_crop.js
@@ -2,88 +2,84 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const sharp = require('sharp');
-const hash = require('object-hash');
 const imageSize = require('image-size');
 
-const ratios = { '1:1': 1, '16:9': 0.5625, '9:16': 1.7777, '4:3': 0.75, '3:4': 1.3333 }
+const ratios = { 
+  '1:1': 1, 
+  '4:3': 0.75, 
+  '3:4': 1.3333,
+  '16:9': 0.5625, 
+  '9:16': 1.7777, 
+}
 
 function sort(obj) {
-  var ordered = {};
-  Object.keys(obj).sort().forEach(function(key) {
+  let ordered = {};
+  for (const key in obj) {
     ordered[key] = obj[key];
-  });
+  }
   return ordered;
 }
 
+function hash(str) {
+  return str.split('').reduce((a, b) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
+}
+
 http.createServer((request, response) => {
-  
-  response.writeHead(200, {'Content-Type': 'image/webp'})
+
+  response.writeHead(200, { 'Content-Type': 'image/webp' })
 
   const url_parts = url.parse(request.url, true);
   const query = url_parts.query;
 
-  if (!query.img) {
+  if (!query.src) {
     return
   }
 
-  const img = `./${hash(sort(query))}.webp`;
-  if (fs.existsSync(img)) {
-    fs.createReadStream(img).pipe(response);
-    return
-  }
-
+  const img = `./${hash(JSON.stringify(sort(query)))}.webp`;
   
+  // if (fs.existsSync(img)) {
+  //   fs.createReadStream(img)
+  //     .pipe(response);
+  //   return
+  // }
+
   const params = {};
-  const original = imageSize(`./${query.img}`)  
-  
+  const original = imageSize(`./${query.src}`)
+
   params.width = (query.width) ? parseInt(query.width) : null;
   params.height = (query.height) ? parseInt(query.height) : null;
   params.left = (query.x) ? parseInt(query.x) : null;
   params.top = (query.y) ? parseInt(query.y) : null;
 
   if (params.left && params.top) {
-    
-    const halfWidth = (params.width / 2)
-    const halfHeight = (params.height / 2)
 
-    params.left = params.left - halfWidth
-    params.top = params.top - halfHeight
-    
-    if (params.left < 0) {
-      params.left = 0
-    } 
-    if ((params.left + halfWidth) > original.width) {
-      params.left -= (params.left + halfWidth) - original.width
-    }
-    if (params.top < 0) {
-      params.top = 0
-    }
-    if ((params.top + halfHeight) > original.height) {
-      params.top -= (params.top + halfHeight) - original.height
-    }     
+    // TODO
 
-    sharp(`./${query.img}`)
+    sharp(`./${query.src}`)
       .extract(params)
       .webp()
-      .toFile(img, () => {
+      .toFile(img, (a ,b) => {
+        console.error(a)
         fs.createReadStream(img)
-        .pipe(response);
+          .pipe(response);
       })
 
     return
   }
-  
-  if (query.ratio) {  
+
+  if (query.ratio) {
     params.position = 'center';
+    let ratio = query.ratio;
     if (!params.width && !params.height) {
-      params.height = Math.ceil(original.width * ratios[query.ratio]);
+      params.height = parseInt(original.width * ratios[ratio]);
       params.width = original.width;
     }
-    if (params.width && !params.height) {
-      params.height = Math.ceil(params.width * ratios[query.ratio]);
+    if (params.width && !params.height) { 
+      params.height = parseInt(params.width * ratios[ratio]);
     }
     if (!params.width && params.height) {
-      params.width = Math.ceil(params.height * ratios[query.ratio]);
+      ratio = ratio.split(':').reverse().join(':')
+      params.width = parseInt(params.height * ratios[ratio]); 
     }
   }
 
@@ -91,7 +87,7 @@ http.createServer((request, response) => {
     params.position = sharp.strategy.attention;
   }
 
-  sharp(`./${query.img}`)
+  sharp(`./${query.src}`)
     .resize(params)
     .webp()
     .toFile(img, () => {
