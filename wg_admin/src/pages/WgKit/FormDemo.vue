baef957035516838c46e38cabced5651c4696dd8
brunothelima <brunothelima@gmail.com>
2019-04-10 17:17:52 -0300

    Binding vuelidate to the wg-form schema

---
+++ wg_admin/src/pages/WgKit/FormDemo.vue
@@ -0,0 +1,57 @@
+<i18n>
+{
+  "pt": {
+    "title": "Formulário",
+    "form": {
+      "first-name": {
+        "label": "Seu nome",
+        "placeholder": "Escreva aqui"
+      }
+    }
+  },
+  "en": {
+    "title": "Form inputs",
+    "form": {
+      "first-name": {
+        "label": "First name",
+        "placeholder": "Type heere"
+      }
+    }
+  }
+}
+</i18n>
+
+<template>
+  <section class="form-demo">
+    <h1>{{ $t('title') }}</h1>
+    <hr />
+    <wg-form :schema="schema" :i18n="$i18n"/>
+  </section>
+</template>
+
+<script> 
+export default {
+  name: 'FormDemo',
+  data() {
+    return {
+      schema: [
+        {
+          type: 'text',
+          name: 'first_name',
+          value: 'Bruno Lima',
+          label: 'form.first-name.label',
+          placeholder: 'form.first-name.placeholder'
+        }
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.fieldset {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2rem;
+}
+</style>
--- wg_admin/src/pages/WgKit/index.vue
+++ wg_admin/src/pages/WgKit/index.vue
@@ -6,22 +6,7 @@
       <text-demo />
       <buttons-demo />
       <images-demo />
-      
-      <div class="field">
-        <label class="field__label" for="">Input name</label>
-        <div class="field__input">
-          <input type="text" placeholder="Type here">
-        </div>
-        <ul class="field__errors">
-          <li>Invalid input value</li>
-        </ul>
-      </div>
-
-      <br />
-      <br />
-      <br />
-      <br />
-
+      <form-demo />
       <page-settings />
     </container>
   </div>
@@ -33,6 +18,7 @@ import ColorsDemo from './ColorsDemo/';
 import TextDemo from './TextDemo/';
 import ButtonsDemo from './ButtonsDemo/';
 import ImagesDemo from './ImagesDemo';
+import FormDemo from './FormDemo';
 
 export default {
 	name: 'WgKit',
@@ -41,57 +27,18 @@ export default {
     ColorsDemo,
     TextDemo,
     ButtonsDemo,
-    ImagesDemo
+    ImagesDemo,
+    FormDemo,
   },
 }
 </script>
+
 <style lang="scss" scoped>
 .wg-kit {
   box-sizing: border-box;
   padding: 2rem 0;
   section, .page-settings{
     margin-bottom: 10rem;
-  }
-  
-  .field {
-    display: flex;
-    flex-flow: column;
-    padding: 1rem;
-    &__label {
-      margin-bottom: 0.5rem;
-      font-size: $font-size-xxs; 
-      font-weight: 600;
-      text-transform: uppercase;
-    }
-    &__input {
-      margin-left: -1rem;
-      margin-bottom: 0.5rem;
-    }
-    &__errors {
-      padding: 0;
-      margin: 0;
-      list-style: none;
-      li {
-        color: $color-error;
-      }
-    }
-  }
-
-  input {
-    padding: 1rem;
-    border-radius: 0.5rem;
-    border: 1px solid var(--color-x-8);
-    background-color: var(--color-x-11);
-    color: var(--color-x-3);
-    outline: none;
-    @include default-transition('border-color, border-width');
-    &::placeholder {
-      color: var(--color-x-7);
-    }
-    &:focus {
-      border: 2px solid var(--color-x-2);
-      padding: calc(1rem - 1px);
-    }
   }
 }
 </style>
